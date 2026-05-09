97ac0cf4872ec8c58c73072ef6e9241998ebd28f
marlonCHK <marlon.kruchinski.chaves@gmail.com>
2026-05-09 17:26:52 -0300

    configurações iniciais da API

---
+++ backend/CadastroDeClientes/CadastroDeClientes/Controllers/ClientesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroDeClientes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok("API funcionando");
+        }
+    }
+}
--- backend/CadastroDeClientes/CadastroDeClientes/Program.cs
+++ backend/CadastroDeClientes/CadastroDeClientes/Program.cs
@@ -1,20 +1,32 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// CORS
+builder.Services.AddCors(p => p.AddPolicy("corsapp", policy =>
+{
+    policy.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("corsapp");
 
 app.UseAuthorization();
 
